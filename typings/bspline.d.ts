6682dee95931cde74e6a3a59fca2731eb51d40c1
Tim Mensch <tim-git-tecra@bitgems.com>
2017-02-28 17:58:04 -0700

    Fix typings.

--- typings/bspline.d.ts
+++ typings/bspline.d.ts
@@ -1,5 +1,9 @@
-declare class BSpline {
-    constructor(degree: number,points: Array<Array<number>>,
-        knots?: Array<number>, weights?: Array<number>);
-    getPoint(t: number, result?: Array<number>): Array<number>;
+declare module "b-spline" {
+
+    export class BSpline {
+        constructor(degree: number, points: Array<Array<number>>,
+            knots?: Array<number>, weights?: Array<number>);
+        getPoint(t: number, result?: Array<number>): Array<number>;
+    }
+
 }
